d25674443e14a3de59cfa25c66cbff17287734b3
Rafael Rabelo <rafael.rabelo@itarget.com.br>
2025-09-29 21:17:04 -0300

    Remove configurações experimentais do Next.js, ajusta scripts no package.json, simplifica a configuração do PostCSS e atualiza estilos globais, alterando variáveis de cores para um novo esquema visual.

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
   images: {
     remotePatterns: [
       {
